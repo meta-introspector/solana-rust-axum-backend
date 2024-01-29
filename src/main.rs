ece6e9d5124b6507deccf318b6daac0e68c18834
Bunny Bites <s.mukeshkr481@gmail.com>
2024-01-29 18:20:20 +0530

    add basic util function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use axum::{routing::get, Router};
+use dotenv::dotenv;
+
+mod util;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     // build our application with a single route
     let app = Router::new()
-        .route("/get", get(plain_text));
+        .route("/get", get(get_pubkey))
+        .route("/getBalance", get(|| get_balance(true)));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -14,6 +20,10 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn plain_text() -> &'static str {
-    "foo"
+async fn get_pubkey() -> String {
+    util::basic_util::get_pubkey("MY_PUB_KEY").to_string()
+}
+
+async fn get_balance(is_allow: bool) {
+    println!("{}", is_allow);
 }
---
+++ src/util/basic_util.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::str::FromStr;
+use solana_sdk::pubkey::Pubkey;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::commitment_config::CommitmentConfig;
+
+pub fn get_pubkey(env_key: &str) -> Pubkey {
+    Pubkey::from_str(env::var(env_key)
+    .expect("Error finding the public key")
+        .as_str())
+    .expect("Error getting the public key")
+}
+
+#[allow(dead_code)]
+pub fn get_client() -> RpcClient {
+    let rpc_url = String::from("https://api.devnet.solana.com"); // JSON RPC URL for devnet
+    RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed())
+}
